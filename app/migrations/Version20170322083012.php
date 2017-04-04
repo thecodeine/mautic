698f47e280a78273afa9f79ebb954fa6c46a6908
mjaniszewski <mateusz@juice.pl>
2017-04-04 12:25:13 +0200

    Fixes migration

--- app/migrations/Version20170322083012.php
+++ app/migrations/Version20170322083012.php
@@ -2,14 +2,28 @@
 
 namespace Mautic\Migrations;
 
-use Doctrine\DBAL\Migrations\AbstractMigration;
+use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
 use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Migrations\SkipMigrationException;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20170322083012 extends AbstractMigration
+class Version20170322083012 extends AbstractMauticMigration
 {
+    /**
+     * @param Schema $schema
+     *
+     * @throws SkipMigrationException
+     * @throws \Doctrine\DBAL\Schema\SchemaException
+     */
+    public function preUp(Schema $schema)
+    {
+        if ($schema->hasTable($this->prefix.'campaign_event_daily_send_log')) {
+            throw new SkipMigrationException('Schema includes this migration');
+        }
+    }
+
     /**
      * @param Schema $schema
      */
@@ -18,9 +32,9 @@ class Version20170322083012 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE campaign_event_daily_send_log (id INT AUTO_INCREMENT NOT NULL, event_id INT NOT NULL, sent_count INT NOT NULL, date DATE DEFAULT NULL, INDEX IDX_1DE0BCA71F7E88B (event_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE campaign_event_daily_send_log ADD CONSTRAINT FK_1DE0BCA71F7E88B FOREIGN KEY (event_id) REFERENCES campaign_events (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE campaign_lead_event_log ADD is_queued TINYINT(1) NOT NULL');
+        $this->addSql('CREATE TABLE '.$this->prefix.'campaign_event_daily_send_log (id INT AUTO_INCREMENT NOT NULL, event_id INT NOT NULL, sent_count INT NOT NULL, date DATE DEFAULT NULL, INDEX '.$this->generatePropertyName('campaign_event_daily_send_log', 'idx', ['event_id']).' (event_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE '.$this->prefix.'campaign_event_daily_send_log ADD CONSTRAINT '.$this->generatePropertyName('campaign_event_daily_send_log', 'fk', ['event_id']).' FOREIGN KEY (event_id) REFERENCES '.$this->prefix.'campaign_events (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE '.$this->prefix.'campaign_lead_event_log ADD is_queued TINYINT(1) NOT NULL');
     }
 
     /**
@@ -31,7 +45,7 @@ class Version20170322083012 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE campaign_event_daily_send_log');
-        $this->addSql('ALTER TABLE campaign_lead_event_log DROP is_queued');
+        $this->addSql('DROP TABLE '.$this->prefix.'campaign_event_daily_send_log');
+        $this->addSql('ALTER TABLE '.$this->prefix.'campaign_lead_event_log DROP is_queued');
     }
 }

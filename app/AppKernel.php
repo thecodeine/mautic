e1dea6a385bbccaca2135669ceb596eb6575a296
Tomasz Kulka <kulka.tomek@gmail.com>
2017-05-09 15:31:28 +0200

    v2.6.2-thecodeine.3

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -51,7 +51,7 @@ class AppKernel extends Kernel
      *
      * @const string
      */
-    const EXTRA_VERSION = '-thecodeine.2';
+    const EXTRA_VERSION = '-thecodeine.3';
 
     /**
      * @var array

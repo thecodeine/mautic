c24f3423e5e9ea9b71d8856b82f112ea4a106115
Mateusz Janiszewski <mateusz@juice.pl>
2017-05-31 09:44:43 +0200

    Pipedrive plugin - cleanup

--- app/AppTestKernel.php
+++ app/AppTestKernel.php
@@ -7,9 +7,7 @@ class AppTestKernel extends AppKernel
      */
     public function getLocalParams()
     {
-        return [
-            'secret_key' => '68c7e75470c02cba06dd543431411e0de94e04fdf2b3a2eac05957060edb66d0',
-        ];
+        return [];
     }
 
     /**
